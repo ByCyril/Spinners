7f1684f7baeb3a7f093fb7395821bdbe0b02a725
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-04-06 14:29:45 -0700

    Initial Commit

---
+++ Spinners/Spinners.h
@@ -0,0 +1,19 @@
+//
+//  Spinners.h
+//  Spinners
+//
+//  Created by Cyril Garcia on 4/6/18.
+//  Copyright © 2018 By Cyril. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spinners.
+FOUNDATION_EXPORT double SpinnersVersionNumber;
+
+//! Project version string for Spinners.
+FOUNDATION_EXPORT const unsigned char SpinnersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spinners/PublicHeader.h>
+
+
---
+++ SpinnersTests/SpinnersTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpinnersTests.swift
+//  SpinnersTests
+//
+//  Created by Cyril Garcia on 4/6/18.
+//  Copyright © 2018 By Cyril. All rights reserved.
+//
+
+import XCTest
+@testable import Spinners
+
+class SpinnersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
